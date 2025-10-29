8d7db94427e86ea85b46fa66bd46dade5e41a8ba
KariminK <103779342+KariminK@users.noreply.github.com>
2025-10-29 23:10:27 +0100

    logo added

--- app/components/modules/Navbar.tsx
+++ app/components/modules/Navbar.tsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router";
 const Navbar = () => {
   return (
     <nav className="max-w-7xl mx-auto py-4 px-2 flex justify-between items-center border-b border-b-neutral-800">
-      <p className="font-bold">
-        Nau<span className="text-green-400">karimin</span>
-      </p>
+      <div className="flex items-center">
+        <img src="/logo.png" alt="" className="w-10" />
+      </div>
       <ul className="flex gap-5 text-neutral-300 items-center">
         <li>
           <NavLink to={"/python"}>Python</NavLink>
